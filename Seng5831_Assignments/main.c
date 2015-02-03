416a5301bc9528d28523a9e239a54783ec7ae0e3
Edward Castillo <edwardcastillo@gmail.com>
2015-02-03 04:42:34 -0600

    Hwk1.1 add the ability to toggle the blinking of the leds.

--- Seng5831_Assignments/main.c
+++ Seng5831_Assignments/main.c
@@ -1,81 +1,68 @@
 /* Seng5831_Assignments - an application for the Pololu Orangutan SVP
- *
- * This application uses the Pololu AVR C/C++ Library.  For help, see:
- * -User's guide: http://www.pololu.com/docs/0J20
- * -Command reference: http://www.pololu.com/docs/0J18
- *
- * Created: 1/31/2015 2:39:21 PM
- *  Author: US296865
- */
+*
+* This application uses the Pololu AVR C/C++ Library.  For help, see:
+* -User's guide: http://www.pololu.com/docs/0J20
+* -Command reference: http://www.pololu.com/docs/0J18
+*
+* Created: 1/31/2015 2:39:21 PM
+*  Author: US296865
+*/
 
 #include <pololu/orangutan.h>
 
-// Blinks the LEDs
-void blink_leds()
-{
-	red_led(1);               // red LED on
-	delay_ms(1000);           // waits for a second
-	red_led(0);               // red LED off
-	delay_ms(1000);           // waits for a second
-	green_led(1);             // green LED on (will not work on the Baby Orangutan)
-	delay_ms(500);            // waits for 0.5 seconds
-	green_led(0);             // green LED off (will not work on the Baby Orangutan)
-	delay_ms(500);            // waits for 0.5 seconds
-}
-
-// toggle the green LED
-void start_green_led_blink()
-{
-	green_led(1);             // green LED on
-	delay_ms(1000);           // waits for a second
-	green_led(0);             // green LED off
-	delay_ms(500);           // waits for half a second
-}
-
-// Toggle the red LED
-void start_red_led_blink()
-{
-	red_led(1);               // red LED on
-	delay_ms(1000);           // waits for a second
-	red_led(0);               // red LED off
-	delay_ms(500);           // waits for half a second
-}
-
-
 int main()
 {
-	print("LED Blink");
-	blink_leds();
+	unsigned long blink_rate = 500;
+	unsigned char toggle_green = 0;
+	unsigned char toggle_red = 0;
+	unsigned char green_state = 0;
+	unsigned char red_state = 0;
+	
+	time_reset();
+	unsigned long mytimer = get_ms();
+	
 	clear();
-	print("Top or Bottom");
-	lcd_goto_xy(0,1);
-	print("Green or Red");
+	print("Ready");
 	while(1)
 	{
-		// wait for either the top or bottom buttons to be pressed
+		// when either the top or bottom buttons is pressed
 		// store the value of the pressed button in the variable 'button'
-		unsigned char button = wait_for_button_press(TOP_BUTTON | BOTTOM_BUTTON);
-		clear();
-		if (button == TOP_BUTTON)     // display the button that was pressed
+		unsigned char button = get_single_debounced_button_press(ANY_BUTTON);
+		
+		// Evaluate pressed button
+		// change led state then set led to that state
+		if (button & TOP_BUTTON)
 		{
-			clear();
-			lcd_goto_xy(0,0);
-			print("Green Light");
-			start_green_led_blink(button);
+			toggle_green=!toggle_green;
+			green_led(toggle_green);
 		}
-		else
+		if (button & BOTTOM_BUTTON)
 		{
-			clear();
-			print("Red Light");
-			start_red_led_blink(button);
+			toggle_red=!toggle_red;
+			red_led(toggle_red);
 		}
 		
-		if (!button_is_pressed(TOP_BUTTON) & !button_is_pressed(BOTTOM_BUTTON))
+		// when the toggle value is active 
+		// blink the led
+		if (toggle_green && (mytimer >= blink_rate))
 		{
-			clear();
-			print("Top or Bottom");
-			lcd_goto_xy(0,1);
-			print("Green or Red");
+			green_state = !green_state;
+			green_led(green_state);
+		}		
+		if (toggle_red && (mytimer >= blink_rate))
+		{
+			red_state = !red_state;
+			red_led(red_state);
 		}
+		
+		// reset timer after the limit has been breached
+		if (mytimer >= blink_rate)
+		{
+			time_reset();
+		}
+
+		// increment timer
+		mytimer = get_ms();
+		
 	}
 }
